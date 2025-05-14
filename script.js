892a4dad2da4522af526cd85bc3c2a2eb255d6d5
Hanna Velychko <hannavelychko3@gmail.com>
2025-05-14 01:23:44 +0100

    API added

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-function generateJoke(e) {
-  e.preventDefault();
-
+function displayJoke(response) {
   new Typewriter("#joke-text", {
-    strings: "Funny Joke",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "😂",
   });
+}
+function generateJoke(e) {
+  e.preventDefault();
+  let topic = document.getElementById("topic");
+  let apiKey = "e3ob1a5bf8tfc2b1608935b2942bc5dc";
+  let context = "You are a comedian telling short jokes";
+  let prompt = `Tell a joke about ${topic.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiURL).then(displayJoke);
 }
 
 let jokeGenerator = document.getElementById("joke-generator");
